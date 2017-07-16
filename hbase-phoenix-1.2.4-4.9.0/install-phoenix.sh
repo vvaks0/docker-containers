7f9739d502ce9a089f7ef7698b08642583fa70a6
vvaks <vvaks@hortonworks.com>
2017-07-15 23:36:03 -0400

    hbase pheonix no zookeeper

--- hbase-phoenix-1.2.4-4.9.0/install-phoenix.sh
+++ hbase-phoenix-1.2.4-4.9.0/install-phoenix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --quiet http://mirror.cogentco.com/pub/apache/phoenix/apache-phoenix-4.9.0-HBase-1.2/bin/apache-phoenix-4.9.0-HBase-1.2-bin.tar.gz
+wget --quite http://mirror.cogentco.com/pub/apache/phoenix/apache-phoenix-4.9.0-HBase-1.2/bin/apache-phoenix-4.9.0-HBase-1.2-bin.tar.gz
 tar -xzf apache-phoenix-4.9.0-HBase-1.2-bin.tar.gz
 rm -f apache-phoenix-4.9.0-HBase-1.2-bin.tar.gz
 ln -s /opt/apache-phoenix-4.9.0-HBase-1.2-bin /opt/phoenix
--- orientdb-2.2/run-image.sh
+++ orientdb-2.2/run-image.sh
@@ -1,1 +1,1 @@
-docker run -d -p 2424:2424 -p 2480:2480 -h hbase vvaks/orientdb:2.2.14
+docker run -d -p 2424:2424 -p 2480:2480 -h localhost vvaks/orientdb:2.2.14
